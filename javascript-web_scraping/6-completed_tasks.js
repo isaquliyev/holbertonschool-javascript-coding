5fd5fcf79598d27588661abccaab5f006ca3709a
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-15 16:28:50 +0400

    Values which equal to 0 eliminated

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -6,11 +6,13 @@ request(process.argv[2], (err, response, body) => {
   if (err) throw err;
   body = JSON.parse(body);
 
-  const result = body.reduce((x, y) => {
+  let result = body.reduce((x, y) => {
     if (!x[y.userId]) x[y.userId] = 0;
     if (y.completed) x[y.userId] += 1;
     return x;
   }, {});
+
+  result = Object.fromEntries(Object.entries(result).filter(([key, value]) => value > 0));
 
   console.log(result);
 });
