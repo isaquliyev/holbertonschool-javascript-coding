b4d0b0e8b41143003182509a29d9f6a96098f97f
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-15 15:22:18 +0400

    Now, my code is more readable

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,8 +5,8 @@ const request = require('request');
 request(process.argv[2], (err, response, body) => {
   if (err) throw err;
   let count = 0;
-  JSON.parse(body).results.forEach(it => {
-    it.characters.forEach(character => {
+  JSON.parse(body).results.forEach(film => {
+    film.characters.forEach(character => {
       if (character.includes('/18/')) {
         count += 1;
       }
