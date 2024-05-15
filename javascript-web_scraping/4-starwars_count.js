51ae479e086a0167dfde649562deb91d788116c4
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-15 15:21:21 +0400

    Only id checked

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,9 +6,11 @@ request(process.argv[2], (err, response, body) => {
   if (err) throw err;
   let count = 0;
   JSON.parse(body).results.forEach(it => {
-    if (it.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-      count += 1;
-    }
+    it.characters.forEach(character => {
+      if (character.includes('/18/')) {
+        count += 1;
+      }
+    });
   });
   console.log(count);
 });
